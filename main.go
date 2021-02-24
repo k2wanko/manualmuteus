2e06d8dc1fafeb4e4801019c8512d9f3412a08b0
Yusei Yamanaka <yusei1128@gmail.com>
2021-02-24 16:13:49 +0900

    Hello-world for sending text message

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	discordToken         = os.Getenv("DISCORD_TOKEN")
+	discordTextChannelID = os.Getenv("DISCORD_TEXT_CHANNEL_ID")
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+	discord, err := discordgo.New("Bot " + discordToken)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		os.Exit(1)
+	}
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening Discord session,", err)
+		os.Exit(1)
+	}
+	defer func() {
+		if err := discord.Close(); err != nil {
+			fmt.Println("error closing Discord session,", err)
+		}
+	}()
+
+	_, err = discord.ChannelMessageSend(discordTextChannelID, "hello, world!")
+	if err != nil {
+		fmt.Println("error sending message,", err)
+		os.Exit(1)
+	}
 }
